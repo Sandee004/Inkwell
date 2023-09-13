f6c57b41dbe56c593f7ef4db6f59f78eaf66ecaa
Sandee004 <102047643+Sandee004@users.noreply.github.com>
2023-09-13 12:39:05 +0000

    Login complete

--- static/register.js
+++ static/register.js
@@ -1,39 +1,10 @@
-  /*
-  document.addEventListener("DOMContentLoaded", function() {
-        var submitButton = document.querySelector("input[type='submit']");
-        submitButton.addEventListener("click", function(event) {
-       //     event.preventDefault(); // Prevent the default form submission
+function validateForm(event) {
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirm-password").value;
 
-            // Get the password and confirm password fields
-            var password = document.querySelector("input[name='password']").value;
-            var confirm = document.querySelector("input[name='confirm']").value;
-
-            // Check if the password fields match
-            if (password !== confirm) {
+            if (password !== confirmPassword) {
                 alert("Password and Confirm Password do not match.");
-            } else {
-                // Submit the form if the password fields match
-                var form = document.querySelector("form");
-                form.submit();
+                event.preventDefault(); // Prevent form submission
+                return false;
             }
-        });
-    });
-
-*/
-var Btn = document.getElementById('submit');
-
-Btn.addEventListener("submit", function(event) {
-    var password = document.querySelector("input[name='password']").value;
-var confirm = document.querySelector("input[name='confirm']").value;
-    
-    if (password === "" || password.length < 8)
-        event.preventDefault();
-        alert("Password too weak")
-    else if (password !== confirm) {
-        event.preventDefault();
-        alert("Passwords don't match")
-    }
-    else{
-       // window.location.href = "/homepage"
-    }
-})
+}
