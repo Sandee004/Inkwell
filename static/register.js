fc4c3eaa81dbae5d54aa09341b0ce53c0dec370e
Sandee004 <102047643+Sandee004@users.noreply.github.com>
2023-09-18 13:11:25 +0000

    Registration auth done

--- static/register.js
+++ static/register.js
@@ -1,10 +1,11 @@
 function validateForm(event) {
             var password = document.getElementById("password").value;
-            var confirmPassword = document.getElementById("confirm-password").value;
+            var confirm = document.getElementById("confirm-password").value;
 
-            if (password !== confirmPassword) {
+            if (password !== confirm) {
                 alert("Password and Confirm Password do not match.");
                 event.preventDefault(); // Prevent form submission
                 return false;
             }
+    return true;
 }
